b4f4d7d68d0719ea224d000d7f0108bc1f1cef42
jaeseongChoe <jaisung0410@gmail.com>
2023-12-04 19:32:37 +0900

    add springboot 초기상태

---
+++ src/main/java/com/daytrip/getoff/GetoffApplication.java
@@ -0,0 +1,13 @@
+package com.daytrip.getoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetoffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daytrip/getoff/GetoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daytrip.getoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
